a71629fb2f6573bb9eb6a64081dde8991fd44aac
Rawan <rawnq.sb@gmail.com>
2022-12-20 00:22:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+
+  let result
+  if (someValue <= 400) {
+    result = 'This one is on me!'
+  }
+  else if (someValue > 400 && someValue < 2000) {
+    result = 'That will be twenty bucks.'
+  }
+  else if (someValue > 2000) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500) {
+    result = 'No can do.';
+
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  const result = city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return result
+}
+function switchOnCharmFromTip(tip) {
+  // Write your code here!
+  let result
+  switch (tip) {
+    case "generous":
+      result = "Thank you so much."
+      break;
+    case "not as generous":
+      result = "Thank you."
+      break;
+    default:
+      result = "Bye."
+      break;
+  }
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
